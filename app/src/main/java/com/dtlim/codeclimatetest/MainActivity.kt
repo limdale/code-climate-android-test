bcaa9e782f76a549cd11a30151da479e9699fe65
Dale Lim <dale.lim@sentia.com.au>
2018-12-03 10:46:24 +1100

    Added some code smells

--- app/src/main/java/com/dtlim/codeclimatetest/MainActivity.kt
+++ app/src/main/java/com/dtlim/codeclimatetest/MainActivity.kt
@@ -2,11 +2,23 @@ package com.dtlim.codeclimatetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val s = "some string"
+
+        Log.d("test", "test")
+        Log.d("test", "test")
+        Log.d("test", "test")
     }
+}
+
+class Coordinate {
+    val x = 0
+    val y = 0
 }
